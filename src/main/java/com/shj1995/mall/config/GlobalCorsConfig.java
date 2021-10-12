bf92d87513a03f64b5f47c0a352139f319daac4b
石豪杰 <shihaojie0212@gmail.com>
2021-10-12 16:59:29 +0800

    fix 上传文件，允许跨域

--- src/main/java/com/shj1995/mall/config/GlobalCorsConfig.java
+++ src/main/java/com/shj1995/mall/config/GlobalCorsConfig.java
@@ -4,21 +4,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-/**
- * @author Jax-li
- */
 @Component
 public class GlobalCorsConfig implements WebMvcConfigurer {
 
-    private static final String MAX_AGE = "18000L";
+    private static final long MAX_AGE = 18000L;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
                 .allowedHeaders("*")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .maxAge(MAX_AGE);
+
     }
 }
